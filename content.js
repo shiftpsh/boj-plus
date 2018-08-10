0ebdbda8a2015619b6bc1d325e6dcd712ac08ef5
shift <me@shiftpsh.com>
2018-08-10 11:57:38 +0900

    Added percentile

--- content.js
+++ content.js
@@ -31,6 +31,29 @@ function getDom(url, callback) {
     };
     request.send();
 }
+function formatPercentage(x) {
+    if (x > 100) {
+        return "100.0";
+    } else if (x > 20) {
+        return x.toFixed(1);
+    } else if (x > 2) {
+        return x.toFixed(2);
+    } else if (x > 0.2) {
+        return x.toFixed(3);
+    } else if (x > 0.02) {
+        return x.toFixed(4);
+    } else if (x > 0.002) {
+        return x.toFixed(5);
+    } else {
+        return x.toFixed(6);
+    }
+}
+
+var leastRank = 75876;
+getDom("https://www.acmicpc.net/user/00076387", function (dom) {
+    const statics = dom.querySelector("table#statics");
+    leastrank = statics.querySelector("td").innerHTML;
+});
 
 var userLinks = document.getElementsByTagName("a");
 userLinks = [].slice.call(userLinks, 0);
@@ -42,12 +65,13 @@ userLinks
         getDom("https://www.acmicpc.net" + item.getAttribute("href"), function (dom) {
             const statics = dom.querySelector("table#statics");
             const rank = statics.querySelector("td").innerHTML;
+            const percent = rank / leastRank * 100;
 
             item.setAttribute("rel", "tooltip");
             item.setAttribute("data-placement", "right");
             item.setAttribute("data-html", "true");
             item.setAttribute("class", "tooltip-click");
-            item.setAttribute("title", "<b>#" + rank + "</b> (상위 " + "%)");
+            item.setAttribute("title", "<b>#" + rank + "</b> (상위 " + formatPercentage(percent) + "%)");
 
             $("[rel=tooltip]").tooltip();
             $("[rel=tooltip]").click(function () {
