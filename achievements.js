a375d6ae72632df1d839eac2537957aeb11968bf
Suhyun Park <arom303@gmail.com>
2018-09-19 12:38:07 +0900

    Oops

--- achievements.js
+++ achievements.js
@@ -4,5 +4,5 @@ achievementLi.innerHTML = "<a href=\"/achievements\">도전과제</a>";
 const dividerLi = document.createElement("LI");
 dividerLi.className = "topbar-devider";
 
-navigation.insertBefore(achievementLi, navigation.childNodes[4]);
-navigation.insertBefore(dividerLi, navigation.childNodes[5]);
+// navigation.insertBefore(achievementLi, navigation.childNodes[4]);
+// navigation.insertBefore(dividerLi, navigation.childNodes[5]);
