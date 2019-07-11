adbc5eec064cd70ad8004807f7e79f69059e70e9
Muhun Kim <iam@muhun.kim>
2019-07-11 14:53:52 +0900

    ES6+ 문법 및 일반적인 JS 관행 따름
    ## 템플릿 구문
    
    \`backtick\` 형식의 템플릿 구문을 권장합니다.
    
    ## 문자열 표기법
    
    DOM 개체 속성의 값에선 `"` 쌍따옴표를 사용하고, JS 문자열에서는 `'` 홑 따옴표를 사용하는 것이 관행입니다.
    
    > 참고 : [Single quotes are more common - Should I use 'single' or "double-quotes" for strings in JavaScript](https://bytearcher.com/articles/single-or-double-quotes-strings-javascript/#single-quotes-are-more-common)
    
    ## 화살표 함수
    
    Java 8+의 그것과 비슷합니다.

--- content.js
+++ content.js
@@ -1,10 +1,9 @@
 function getJson(url, callback) {
-    var request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
-            var data = JSON.parse(request.responseText);
-            callback(data);
+            callback(JSON.parse(request.responseText));
         } else {
             // TODO
         }
@@ -16,11 +15,11 @@ function getJson(url, callback) {
 }
 
 function getDom(url, callback) {
-    var request = new XMLHttpRequest();
+    const request = new XMLHttpRequest();
     request.open('GET', url, true);
     request.onload = function () {
         if (request.status >= 200 && request.status < 400) {
-            var el = document.createElement('html');
+            const el = document.createElement('html');
             el.innerHTML = request.responseText;
             callback(el);
         } else {
@@ -34,66 +33,60 @@ function getDom(url, callback) {
 }
 
 function formatPercentage(x) {
-    if (x > 100) {
-        return "100.0";
-    } else if (x > 20) {
+    if (x > 100)
+        return '100.0';
+    if (x > 20)
         return x.toFixed(1);
-    } else if (x > 2) {
+    if (x > 2)
         return x.toFixed(2);
-    } else if (x > 0.2) {
+    if (x > 0.2)
         return x.toFixed(3);
-    } else if (x > 0.02) {
+    if (x > 0.02)
         return x.toFixed(4);
-    } else if (x > 0.002) {
+    if (x > 0.002)
         return x.toFixed(5);
-    } else {
-        return x.toFixed(6);
-    }
+	return x.toFixed(6);
 }
 
-var leastRank = 75876;
-getDom("https://www.acmicpc.net/user/00076387", function (dom) {
-    const statics = dom.querySelector("table#statics");
-    leastrank = statics.querySelector("td").innerHTML;
+let leastRank = 75876;
+getDom('https://www.acmicpc.net/user/00076387', dom => {
+    const statics = dom.querySelector('table#statics');
+    leastrank = statics.querySelector('td').innerHTML;
 });
 
-var userLinks = document.getElementsByTagName("a");
+let userLinks = document.getElementsByTagName("a");
 userLinks = [].slice.call(userLinks, 0);
 userLinks
-    .filter(function (item) {
-        return item.getAttribute("href") && item.getAttribute("href").startsWith("/user/")
-    })
-    .forEach(function (item, index) {
-        const user = item.getAttribute("href").split("/")[2];
+    .filter(item =>  item.getAttribute('href') && item.getAttribute('href').startsWith('/user/'))
+    .forEach((item, index) => {
+        const user = item.getAttribute('href').split('/')[2];
 
-        getDom("https://www.acmicpc.net" + item.getAttribute("href"), function (dom) {
-            const statics = dom.querySelector("table#statics");
-            const solved = statics.querySelector("tr:nth-child(2) td").innerText;
-            const bojRank = statics.querySelector("tr:nth-child(1) td").innerText;
+        getDom(`https://www.acmicpc.net${item.getAttribute('href')}`, dom => {
+            const statics = dom.querySelector('table#statics');
+            const solved = statics.querySelector('tr:nth-child(2) td').innerText;
+            const bojRank = statics.querySelector('tr:nth-child(1) td').innerText;
             const bojPercent = bojRank / leastRank * 100;
 
-            item.setAttribute("rel", "tooltip");
-            item.setAttribute("data-placement", "right");
-            item.setAttribute("data-html", "true");
-            item.setAttribute("class", "tooltip-click");
-            item.setAttribute("title",
-                "<table style=\"text-align:left; border-spacing: 4px; border-collapse: separate;\">" +
-                "<tr>" +
-                "<td>Solved </td><td><b>" + solved + "</b></td><td></td>" +
-                "</tr>" +
-                "<tr>" +
-                "<td>BOJ </td><td><b>#" + bojRank + "</b></td><td> (상위 " + formatPercentage(bojPercent) + "%)</td>" +
-                "</tr>" +
-                "</table>");
+            item.setAttribute('rel', 'tooltip');
+            item.setAttribute('data-placement', 'right');
+            item.setAttribute('data-html', 'true');
+            item.setAttribute('class', 'tooltip-click');
+            item.setAttribute('title',
+                `<table style="text-align:left; border-spacing: 4px; border-collapse: separate;">
+                	<tr>
+						<td>Solved </td>
+						<td><b>${solved}</b></td>
+						<td></td>
+                	</tr>
+                	<tr>
+						<td>BOJ</td>
+						<td><b>#${bojRank}</b></td>
+						<td> (상위 ${formatPercentage(bojPercent)}%)</td>
+                	</tr>
+                </table>`);
 
-            $("[rel=tooltip]").tooltip();
-            $("[rel=tooltip]").click(function () {
-                if ($(this).hasClass('tooltip-click')) {
-                    return true;
-                } else {
-                    return false;
-                }
-            });
+            $('[rel=tooltip]').tooltip();
+            $('[rel=tooltip]').click(self => $(self.currentTarget).hasClass('tooltip-click'));
         });
     })
 
